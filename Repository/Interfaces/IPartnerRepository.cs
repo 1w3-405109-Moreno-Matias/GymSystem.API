c50c98ff32f9553a3a339e4b59cee09e16713083
Matias Moreno <405109@tecnicatura.frc.utn.edu.ar>
2025-11-19 14:30:09 -0300

    feat: add IPartnerRepository

---
+++ Repository/Interfaces/IPartnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using GymSystem.Api.Models;
+
+namespace GymSystem.Api.Repository.Interfaces
+{
+    public interface IPartnerRepository
+    {
+        List<Partner> GetALL();
+        Partner GetByNumberDocument(int partnerNumberDocument);
+        bool Add(Partner AddPartner);
+        bool Update(Partner UpdatePartner);
+
+        //delete baja lógica (suspendido o dado de baja)
+    }
+}
