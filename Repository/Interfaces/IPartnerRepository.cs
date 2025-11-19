555dc68827e65ecac2a02317411d9d46ff5938f9
Matias Moreno <405109@tecnicatura.frc.utn.edu.ar>
2025-11-19 15:05:17 -0300

    chore: confing IPartenerRpeository Update

--- Repository/Interfaces/IPartnerRepository.cs
+++ Repository/Interfaces/IPartnerRepository.cs
@@ -7,7 +7,7 @@ namespace GymSystem.Api.Repository.Interfaces
         List<Partner> GetALL();
         Partner GetByNumberDocument(int partnerNumberDocument);
         bool Add(Partner AddPartner);
-        bool Update(Partner UpdatePartner);
+        bool Update(Partner UpdatePartner, int numberDocument);
 
         //delete baja lógica (suspendido o dado de baja)
     }
