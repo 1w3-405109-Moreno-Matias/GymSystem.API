1313dc3364f42354abcdf27ec563ababbe7ee537
Matias Moreno <405109@tecnicatura.frc.utn.edu.ar>
2025-11-19 11:07:18 -0300

    feat: add Plan model

---
+++ Models/Plan.cs
@@ -0,0 +1,10 @@
+﻿namespace GymSystem.Api.Models
+{
+    public class Plan
+    {
+        public int PlanId { get; set; }
+        public string Name { get; set; }
+        public decimal MonthlyPrice { get; set; }
+        public ICollection<Partner> Partners { get; set; }
+    }
+}
