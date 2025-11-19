4271c220f44f6cc3710f1b3215596f81fc89f782
Matias Moreno <405109@tecnicatura.frc.utn.edu.ar>
2025-11-19 11:52:00 -0300

    feat: add precision to MonthlyPrice and Update database

--- Models/Partner.cs
+++ Models/Partner.cs
@@ -2,7 +2,7 @@
 {
     public class Partner
     {
-        public int IdPartner { get; set; }
+        public int PartnerId { get; set; }
         public string Name { get; set; }
         public string LastName{ get; set; }
         public int DocumentNumber { get; set; }
--- Models/Plan.cs
+++ Models/Plan.cs
@@ -1,9 +1,13 @@
-﻿namespace GymSystem.Api.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymSystem.Api.Models
 {
     public class Plan
     {
         public int PlanId { get; set; }
         public string Name { get; set; }
+        
+        [Precision(10, 2)]
         public decimal MonthlyPrice { get; set; }
         public ICollection<Partner> Partners { get; set; }
     }
