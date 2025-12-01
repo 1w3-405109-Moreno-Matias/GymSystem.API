8e72895f5d30653c271974007f2ce7ad539b3b74
Matias Moreno <405109@tecnicatura.frc.utn.edu.ar>
2025-11-30 21:14:45 -0300

    feat: add PartnerCreateDto

---
+++ DTOs/PartnerCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GymSystem.Api.DTOs
+{
+    public class PartnerCreateDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int DocumentNumber { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int PlanId { get; set; }
+    }
+}
